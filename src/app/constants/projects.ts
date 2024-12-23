b9fbd37b6d021ae5f6c7a55a9254b42fa7dcce33
Khaled AlRashidi <khaled.rashidi99@outlook.com>
2024-12-23 16:12:58 +0300

    Added 4 Projects

--- src/app/constants/projects.ts
+++ src/app/constants/projects.ts
@@ -85,4 +85,69 @@ export const projects = [
       "/images/projects/ihms-app/10.webp",
     ],
   },
+  {
+    cardName: "iCMS App",
+    cardBackground: "/images/projects/cms-app/main.webp",
+    cardDetails:
+      "A content management system app for mobiles and accessories, with the ability to add fake data for products, accessories, and offers. The app loads data from a JSON file, and the data is updated in real-time.",
+    cardStack: "ReactJS | TS | ContextAPI | React-Router | MUI",
+    projectGit: "https://github.com/khaled-rashidi99/Company-CMS",
+    projectDemo: "https://icompany-cms.vercel.app/",
+    projectImages: [
+      "/images/projects/cms-app/main.webp",
+      "/images/projects/cms-app/1.webp",
+      "/images/projects/cms-app/2.webp",
+      "/images/projects/cms-app/3.webp",
+      "/images/projects/cms-app/4.webp",
+      "/images/projects/cms-app/5.webp",
+      "/images/projects/cms-app/6.webp",
+    ],
+  },
+  {
+    cardName: "Social Media Feed App",
+    cardBackground: "/images/projects/social-app/main.webp",
+    cardDetails:
+      "A social media feed app, with posts integrated from APIs, and the ability to like posts, saved in your profile page, and view each post's comments",
+    cardStack:
+      "ReactJS | TS | Redux | React-Router | Memoization and Lazy Loading | Pagination | Shadcn UI | TailwindCSS ",
+    projectGit: "https://github.com/khaled-rashidi99/Social-App",
+    projectDemo: "https://isocial-feed-app.vercel.app/",
+    projectImages: [
+      "/images/projects/social-app/main.webp",
+      "/images/projects/social-app/1.webp",
+      "/images/projects/social-app/2.webp",
+    ],
+  },
+  {
+    cardName: "Flipping Card Game",
+    cardBackground: "/images/projects/flipping-game/main.webp",
+    cardDetails:
+      "A flipping card game, with the ability to play in three difficulties, and to save your win streaks.",
+    cardStack: "Vanilla JavaScript | HTML | CSS | TailwindCSS",
+    projectGit:
+      "https://github.com/khaled-rashidi99/Flipping-Card-Guessing-Game",
+    projectDemo:
+      "https://khaled-rashidi99.github.io/Flipping-Card-Guessing-Game/src/",
+    projectImages: [
+      "/images/projects/flipping-game/main.webp",
+      "/images/projects/flipping-game/1.webp",
+      "/images/projects/flipping-game/2.webp",
+      "/images/projects/flipping-game/3.webp",
+    ],
+  },
+  {
+    cardName: "Landing Page",
+    cardBackground: "/images/projects/landing-page/main.webp",
+    cardDetails:
+      "A landing page for a company, with the ability to view the company's services, and to contact the company's team.",
+    cardStack: "Vanilla JavaScript | HTML | CSS | Bootstrap",
+    projectGit: "https://github.com/khaled-rashidi99/Custom-Landing-Page",
+    projectDemo: "https://khaled-rashidi99.github.io/Custom-Landing-Page/src/",
+    projectImages: [
+      "/images/projects/landing-page/main.webp",
+      "/images/projects/landing-page/1.webp",
+      "/images/projects/landing-page/2.webp",
+      "/images/projects/landing-page/3.webp",
+    ],
+  },
 ];
